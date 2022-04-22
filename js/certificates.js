7c98f73ae9f39a36814af951cbe473953450369a
MCXIV <mcxiv14@gmail.co>
2022-04-22 21:53:43 +0530

    Hello

--- js/certificates.js
+++ js/certificates.js
@@ -1,8 +1,7 @@
 let certificatesDiv = document.getElementById('Certificates');
 
-// document.createElement('img')
-
-certificate_image_names.forEach(value => {
-  let imgTag = document.createElement('img')
-  imgTag.src = "img/certs.idx"
+certificateImageNames.forEach(value => {
+  let imgTag = document.createElement('img');
+  imgTag.src = "img/certs/" + value;
+  certificatesDiv.appendChild(imgTag);
 })
