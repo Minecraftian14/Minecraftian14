92045b60c69e7bbe8b18cf9f8bc122bb1c5bf47c
MCXIV <mcxiv14@gmail.co>
2022-04-22 21:33:13 +0530

    Hello

--- js/certificates.js
+++ js/certificates.js
@@ -2,17 +2,17 @@ let certs = document.getElementById('Certificates');
 
 // document.createElement('img')
 
-let space = document.createElement('img')
-space.src = "img/certs.idx"
+// let space = document.createElement('img')
+// space.src = "img/certs.idx"
 
-// let request = new XMLHttpRequest();
-// request.open("GET", "img/certs.idx", true);
-// request.onreadystatechange = function () {
-//   if (request.readyState === 4) {
-//     if (request.status === 200 || request.status == 0) {
-//       let indices_text = request.responseText;
-//       console.log(indices_text)
-//     }
-//   }
-// }
-// request.send(null);
+let request = new XMLHttpRequest();
+request.open("GET", "img/certs.idx", true);
+request.onreadystatechange = function () {
+  if (request.readyState === 4) {
+    if (request.status === 200 || request.status == 0) {
+      let indices_text = request.responseText;
+      console.log(indices_text)
+    }
+  }
+}
+request.send(null);
