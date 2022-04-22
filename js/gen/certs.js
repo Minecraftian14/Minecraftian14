365704c2f511b7365eaebef761d03ca545e6264c
MCXIV <mcxiv14@gmail.co>
2022-04-22 21:35:42 +0530

    Hello

---
+++ js/gen/certs.js
@@ -0,0 +1,1 @@
+const certificate_image_names = [0001.jpg, 0002.jpg, 0003.jpg, 0004.jpg, 0005.jpg, 0006.jpg, 0007.jpg, 0008.jpg, 0009.jpg, 0010.jpg, 0011.jpg, 0012.jpg, IIHT_Advance_Java.jpg, IIHT_C.jpg, IIHT_Core_Java.jpg, ];
