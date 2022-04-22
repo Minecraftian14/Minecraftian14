2fb560ce7109e14140967040149fcb906dcb96ba
Anirudh Sharma <mcxiv14@gmail.com>
2022-04-22 21:03:49 +0530

    Update certificates.js

--- js/certificates.js
+++ js/certificates.js
@@ -1,5 +1,18 @@
 let certs = document.getElementById('Certificates');
 
-
 // document.createElement('img')
 
+let space = document.createElement('img')
+space.src = "img/certs.idx"
+
+// let request = new XMLHttpRequest();
+// request.open("GET", "img/certs.idx", true);
+// request.onreadystatechange = function () {
+//   if (request.readyState === 4) {
+//     if (request.status === 200 || request.status == 0) {
+//       let indices_text = request.responseText;
+//       console.log(indices_text)
+//     }
+//   }
+// }
+// request.send(null);
